011bd1cb9425d153308ef714c1a515c596fef057
wykhuh <wykhuh@users.noreply.github.com>
2019-06-15 13:09:22 -0700

    reset files

--- src/apollo/client.js
+++
@@ -1,7 +0,0 @@
-import ApolloClient from 'apollo-boost';
-import fetch from 'isomorphic-fetch';
-
-export const client = new ApolloClient({
-  uri: 'https://nx9zvp49q7.lp.gql.zone/graphql',
-  fetch,
-});
