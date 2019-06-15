a68238119f8be22704a524d25f386f490a666740
wykhuh <wykhuh@users.noreply.github.com>
2019-06-15 13:16:45 -0700

    configure graphql

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'RMAPI',
+        fieldName: 'rickAndMorty',
+        url: 'https://rickandmortyapi-gql.now.sh/',
+      },
+    },
+  ],
+}
