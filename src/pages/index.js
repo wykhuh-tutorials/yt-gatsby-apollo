4ddab3f683e51d01487ddb905c23fa8a8158fc8c
wykhuh <wykhuh@users.noreply.github.com>
2019-06-15 13:43:09 -0700

    display Rick and Morty character from server graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default () => (
-  <div>
-    <h1>Hello</h1>
+// when gatsby runs, it wii run this graphql query
+export const GATSBY_QUERY = graphql`
+  {
+    rickAndMorty {
+      character(id: 1) {
+        name
+        image
+      }
+    }
+  }
+`;
+
+// gatsby will pass query in as a data prop
+export default ({
+  data: {
+    rickAndMorty: { character },
+  },
+}) => (
+  <div style={{ textAlign: 'center', width: 600, margin: '50px auto' }}>
+    <h1>Hello {character.name}</h1>
+    <img src={character.image} alt={character.name} style={{ width: 300 }} />
   </div>
 );
