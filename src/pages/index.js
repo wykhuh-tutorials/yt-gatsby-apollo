9350a831e566bb588bcdb3450502aae25e626a06
wykhuh <wykhuh@users.noreply.github.com>
2019-06-15 14:24:14 -0700

    load dog image from client-side apollo query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 // when gatsby runs, it wii run this graphql query
 export const GATSBY_QUERY = graphql`
@@ -13,6 +15,15 @@ export const GATSBY_QUERY = graphql`
   }
 `;
 
+const APOLLO_QUERY = gql`
+  {
+    dog(breed: "frise") {
+      breed
+      src: displayImage
+    }
+  }
+`;
+
 // gatsby will pass query in as a data prop
 export default ({
   data: {
@@ -22,5 +33,16 @@ export default ({
   <div style={{ textAlign: 'center', width: 600, margin: '50px auto' }}>
     <h1>Hello {character.name}</h1>
     <img src={character.image} alt={character.name} style={{ width: 300 }} />
+
+    <Query query={APOLLO_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <span>Loading...</span>;
+        if (error) return <p>{error.message}</p>;
+
+        return (
+          <img src={data.dog.src} alt={data.dog.breed} style={{ width: 300 }} />
+        );
+      }}
+    </Query>
   </div>
 );
